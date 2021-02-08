19eed0378a63844d75fa5268ae8b4bbf3fa9078f
Sang <sangphamyn313@gmail.com>
2021-02-08 14:14:02 +0700

    test 5

--- src/app/controllers/FootballClubController.js
+++ src/app/controllers/FootballClubController.js
@@ -7,7 +7,9 @@ class FootballClubController{
     show(req, res, next) {
         FootballClub.findOne({ slug: req.params.slug })
             .then(footballClub => {
-                res.render('footballClub/show', { footballClub: mongooseToObject(footballClub) });
+                res.render('footballClub/show', {
+                    footballClub: mongooseToObject(footballClub)
+                })
             })
             .catch(next);
     }
